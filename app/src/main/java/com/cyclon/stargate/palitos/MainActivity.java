df13775cd0b179d04d08e4e157f34eea4898076c
Unknown <carlosh2340@gmail.com>
2017-11-20 21:40:41 -0300

    Lógica do jogo finalizada
    Código para definir o resultado do jogo adicionado
    Toast informando o vencedor adicionado
    OBS: só funciona uma rodada

--- app/src/main/java/com/cyclon/stargate/palitos/MainActivity.java
+++ app/src/main/java/com/cyclon/stargate/palitos/MainActivity.java
@@ -9,10 +9,13 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private final static int MAXPALITOS = 6,
+            EMPATE = 0, USERWINS = 1, PCWINS = 2;
     private EditText num;
     private ImageButton pross;
     private ImageView palito;
@@ -24,63 +27,128 @@ public class MainActivity extends AppCompatActivity {
     private int palpPC;
     private int jogUser;
     private int jogPC;
+    int cont = 0;
 
-    private int cont;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        cont = 0;
 
         pross = findViewById(R.id.continuar);//botão continuar
         num = findViewById(R.id.numUser);//edittext
         repetir = findViewById(R.id.repetir);//repetir btão
         textEdit = findViewById(R.id.textEdit);//textos de guia - edit
+        halfBackg = findViewById(R.id.view);
 
 
         num.setHint(getResources().getString(R.string.hintPalpite));
-        textEdit.setText("Digite um palpite de 2 a 12");
-        pross.setColorFilter(Color.rgb(117, 144, 7));//colocar uma cor para cada etapa a ser passada, ex.:Palpite, Jogada, Vitória ou derrota
-        while (cont<2){
-            pross.setOnClickListener(listener);
-            cont++;
-        }
+        textEdit.setText(getResources().getString(R.string.textPalpite));
+        pross.setBackgroundColor(Color.rgb(117, 144, 7));//colocar uma cor para cada etapa a ser passada, ex.:Palpite, Jogada, Vitória ou derrota
+        halfBackg.setBackgroundColor(Color.rgb(117, 144, 7));
 
+        repetir.setOnClickListener(repete);
+        pross.setOnClickListener(listener);
 
-
-
-}
+    }
 
     View.OnClickListener listener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            if(cont == 0){//palpite
-                palpUser = Integer.parseInt(num.getText().toString());
+            switch (cont) {
+                case 0:
+                    palpUser = Integer.parseInt(num.getText().toString()); //palpite
 
-            }else{ //jogada
-                jogUser = Integer.parseInt(num.getText().toString());
+                    break;
+                case 1:
+                    textEdit.setText(R.string.textJogar); //jogada
+                    jogUser = Integer.parseInt(num.getText().toString());
+
+                    jogPC = getJogadaPC(MAXPALITOS);
+                    palpPC = getPalpPC(jogPC, MAXPALITOS);
+
+                    showToast(getWinner());
+
+                    repetir.setVisibility(View.VISIBLE);
+                    textEdit.setVisibility(View.GONE);
+                    num.setVisibility(View.GONE);
+                    pross.setVisibility(View.GONE);
+                    break;
             }
+            cont++;
+        }
+    };
 
+    View.OnClickListener repete = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            num.setVisibility(View.VISIBLE);
+            pross.setVisibility(View.VISIBLE);
+            cont = 0;
+            textEdit.setText(getResources().getString(R.string.textPalpite));
 
         }
     };
 
-    int getPalpPC(int maxPC, int maxUser){
+
+    static int getJogadaPC(int maxPC) {
         Random pc = new Random();
-        Random user = new Random();
-        int palitosPC, palitosUser, palp;
+        int jogadaPC;
 
         //número de palitos que o pc vai jogar
-        palitosPC = (pc.nextInt(maxPC) + 1);
+        jogadaPC = (pc.nextInt(maxPC) + 1);
+
+        return jogadaPC;
+    }
+
+    static int getPalpPC(int jogadaPC, int maxUser) {
+        //Random user = new Random();
+        int jogadaUser, palp;
 
         //chutar o número de palitos que o usuário jogou...
-        palitosUser = (user.nextInt(maxUser) + 1);
+        //palitosUser = (user.nextInt(maxUser) + 1);
         //ou assumir que ele jogou todos?
-        //palitosUser = maxUser
+        jogadaUser = maxUser;
 
-        palp = palitosPC + palitosUser;
+        palp = jogadaPC + jogadaUser;
 
         return palp;
     }
 
+    int getWinner() {
+        int total = jogPC + jogUser;
+
+        //O user acertou e o pc errou ou vice-versa?
+        if (palpUser == total ^ palpPC == total) {
+            //sim. Quem acertou?
+            if (palpUser == total) {
+                //O User
+                return USERWINS;
+            } else {
+                //O PC
+                return PCWINS;
+            }
+        } else {
+            //Não, os dois acertaram ou os dois erraram
+            return EMPATE;
+        }
+    }
+
+    void showToast(int winner){
+        int textId = 0;
+        Toast toast;
+
+        switch (winner){
+            case EMPATE:
+                textId = R.string.toast_empate;
+                break;
+            case USERWINS:
+                textId = R.string.toast_user;
+                break;
+            case PCWINS:
+                textId = R.string.toast_pc;
+        }
+
+        toast = Toast.makeText(this,textId, Toast.LENGTH_SHORT);
+        toast.show();
+    }
 }
